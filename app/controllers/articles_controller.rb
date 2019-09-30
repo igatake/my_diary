c4ee34493fa243f91a38e7da46801d4799244ef2
Taketo Igai <t.igai.716@gmail.com>
2019-09-30 22:24:46 +0900

    eacape 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all.reverse
+    @articles = Article.all.order("id DESC").page(params[:page]).per(5)
   end
 
   # GET /articles/1
